4e4a7b3e2295d25321f6483c0b4d7b23350296c5
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-14 13:45:07 -0500

    Add ShoppingCartIcon

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../Checkout.css';
-
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Subtotal from './Subtotal';
 
 function Checkout() {
@@ -10,7 +10,9 @@ function Checkout() {
         <img src="" alt="" title="" className="checkout__ad" />
       </div>
       <div>
-        <h2 className="checkout__title">Your shopping basket</h2>
+        <h2 className="checkout__title">
+          <ShoppingCartIcon fontSize="large" />&nbsp;
+          Your shopping basket</h2>
         {/* BasketItem */}
       </div>
       <div className="checkout__right">
