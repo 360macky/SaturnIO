6ac63dd4b59437ed660df71b3f16084b24f4fb6f
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-15 17:49:04 -0500

    Add Map function to loop over each Product at Checkout component

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,18 +2,32 @@ import React from 'react';
 import '../Checkout.css';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Subtotal from './Subtotal';
+import CheckoutProduct from './CheckoutProduct';
+
+import { useStateValue } from '../StateProvider';
 
 function Checkout() {
+  const [{ basket }] = useStateValue();
+
   return (
     <div className="checkout">
-      <div className="checkout__left">
-        <img src="" alt="" title="" className="checkout__ad" />
-      </div>
       <div>
         <h2 className="checkout__title">
-          <ShoppingCartIcon fontSize="large" />&nbsp;
-          Your shopping basket</h2>
-        {/* BasketItem */}
+          <ShoppingCartIcon fontSize="large" />
+          &nbsp; Your shopping basket
+        </h2>
+        <div className="checkout-products">
+          {basket.map((product, index) => (
+            <CheckoutProduct
+              key={index}
+              id={product.id}
+              title={product.title}
+              image={product.image}
+              price={product.price}
+              rating={product.rating}
+            />
+          ))}
+        </div>
       </div>
       <div className="checkout__right">
         <Subtotal />
