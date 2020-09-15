2f23d65bb48b940512b637e14899b69fc22153e6
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-15 16:16:20 -0500

    Add REMOVE_FROM_BASKET action/case to the reducer

--- src/reducer.js
+++ src/reducer.js
@@ -2,9 +2,11 @@ export const initialState = {
   basket: [],
 };
 
-export const getBasketTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0)
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
 
 const ADD_TO_BASKET = 'ADD_TO_BASKET';
+const REMOVE_FROM_BASKET = 'REMOVE_FROM_BASKET';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -12,6 +14,24 @@ const reducer = (state, action) => {
       return {
         ...state,
         basket: [...state.basket, action.item],
+      };
+    case REMOVE_FROM_BASKET:
+      const index = state.basket.findIndex(
+        (product) => product.id === action.id
+      );
+      let updatedBasket = [...state.basket];
+
+      if (index >= 0) {
+        updatedBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove the product (id: ${action.id}) as its not in basket!`
+        );
+      }
+
+      return {
+        ...state,
+        basket: updatedBasket,
       };
     default:
       return state;
