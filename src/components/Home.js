7238e0bf972c6e39007a66eaddc7e73994d6bfad
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-16 09:34:59 -0500

    Add Products to the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,22 +9,53 @@ function Home() {
         <div className="home__cover">
           <h1>Everything about astronomy with a click</h1>
         </div>
-        <div class="home__row">
+        <div className="home__row">
           <Product
-            title="The lean startup"
-            price={29.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"
+            id="12321341"
+            title="National Geographic Backyard Guide to the Night Sky, 2nd Edition"
+            price={12.96}
             rating={5}
+            image="https://i.imgur.com/7mdvRqP.jpg"
           />
-          <Product />
+          <Product
+            id="49538094"
+            title="Telescope Star Finder with Tripod F36050 HD Zoom Monocular Space for Kids and Beginner"
+            price={36.0}
+            rating={4}
+            image="https://i.imgur.com/yG2JJBE.jpg"
+          />
         </div>
-        <div class="home__row">
-          <Product />
-          <Product />
-          <Product />
+        <div className="home__row">
+          <Product
+            id="4903850"
+            title="MaxUSee Kids Telescope 400x400mm with Tripod & Finder"
+            price={35.99}
+            rating={3}
+            image="https://i.imgur.com/kfCny95.jpg"
+          />
+          <Product
+            id="23445930"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="3254354345"
+            title="Powerful Binoculars with Clear Weak light Vision - Lightweight"
+            price={58.99}
+            rating={4}
+            image="https://i.imgur.com/ZsgLO4x.jpg"
+          />
         </div>
-        <div class="home__row">
-          <Product />
+        <div className="home__row">
+          <Product
+            id="90829332"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={1094.98}
+            rating={4}
+            image="https://i.imgur.com/J8xQgb8.jpg"
+          />
         </div>
       </div>
     </div>
