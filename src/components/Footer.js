628f4cfd39462885d4d91c0d28d9ad2a36e9e3d5
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-17 12:13:20 -0500

    Add links in Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,38 @@ function Footer() {
   return (
     <footer>
       <div className="footer__first-block">
-        <p>Products</p>
-        <p>License</p>
-        <p>Repository</p>
-        <p>MarceloArias.com</p>
+        <p>
+          <a
+            href="https://github.com/360macky/Saturn/blob/master/LICENSE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            License
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://github.com/360macky/Saturn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Repository
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://marceloarias.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Marcelo Arias portfolio"
+          >
+            MarceloArias.com
+          </a>
+        </p>
       </div>
-      <div className="footer__second-block">
-      </div>
+      <div className="footer__second-block"></div>
     </footer>
-  )
+  );
 }
 
 export default Footer;
