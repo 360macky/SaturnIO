22ae2c8334ab16927a797ae979cbbe4a47c63eeb
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-09-17 11:12:36 -0500

    Create Subtotal component

---
+++ src/components/Subtotal.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../Subtotal.css';
+import CurrencyFormat from 'react-currency-format';
+import CheckIcon from '@material-ui/icons/Check';
+import { useStateValue } from '../StateProvider';
+import { getBasketTotal } from '../reducer';
+
+function Subtotal() {
+  const [{ basket }] = useStateValue();
+
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({basket.length} items): <b>{value}</b>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)}
+        displayType={'text'}
+        thousandSeparator={true}
+        prefix={'$'}
+      />
+
+      <button className="subtotal__checkout-button">
+        <CheckIcon fontSize="large" />
+        &nbsp; Proceed to Checkout
+      </button>
+    </div>
+  );
+}
+
+export default Subtotal;
